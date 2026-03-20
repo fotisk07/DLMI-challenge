5bd8384167fe275dada81ea021d031b95714b163
Fotis <fotiskapotos@gmail.com>
2026-03-20 15:29:02 +0100

    intro notebook

--- data/download.sh
+++ data/download.sh
@@ -1,1 +1,22 @@
- uvx --from git+https://github.com/Kaggle/kaggle-api/ kaggle competitions download -c mva-dlmi-2026-histopathology-ood-classification
+#!/bin/bash
+set -e  # stop on error
+
+
+# Download
+echo "Downloading dataset..."
+uvx --from git+https://github.com/Kaggle/kaggle-api/ \
+    kaggle competitions download -c mva-dlmi-2026-histopathology-ood-classification
+
+# Unzip
+echo "Unzipping..."
+unzip mva-dlmi-2026-histopathology-ood-classification.zip
+
+# Organize
+mkdir -p data
+mv test.h5 train.h5 val.h5 data/
+
+# Cleanup
+echo "Cleaning up..."
+rm mva-dlmi-2026-histopathology-ood-classification.zip
+rm getting_started.ipynb
+echo "Done."
